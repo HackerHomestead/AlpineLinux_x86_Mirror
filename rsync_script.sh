2a472854fe6c00fb00c54e055ffe5f2d83675eda
Handy Hacker <handy@hackerhomestead.com>
2024-10-06 22:01:33 +0000

    Updated rsync script

--- rsync_script.sh
+++ rsync_script.sh
@@ -6,24 +6,19 @@ if [ -z "$flock" ] ; then
   exec env flock=1 flock -n $lockfile "$0" "$@"
 fi
 
-#src=rsync://rsync.alpinelinux.org/alpine/ 
+for dir in releases community main; do
+	src="rsync://rsync.alpinelinux.org/alpine/v3.20/$dir/x86"
+	dest="/home/handy/repo/alpine/$dir/x86"
 
-src=rsync://rsync.alpinelinux.org/alpine/v3.20/releases/x86
-#src=rsync://rsync.alpinelinux.org/alpine/v3.20/community/x86
-#src=rsync://rsync.alpinelinux.org/alpine/v3.20/main/x86
-
-#dest=/var/www/localhost/htdocs/alpine/
-dest=/home/handy/repo/alpine/v3.20/releases/x86/
-
-mkdir -p "$dest"
-/usr/bin/rsync \
-	--progress \
-        --archive \
-        --update \
-        --hard-links \
-        --delete \
-        --delete-after \
-        --delay-updates \
-        --timeout=600 \
-        $exclude \
-        "$src" "$dest"
+	mkdir -p "$dest"
+	/usr/bin/rsync \
+		--progress \
+		--archive \
+		--update \
+		--hard-links \
+		--delete \
+		--delete-after \
+		--delay-updates \
+		--timeout=600 \
+		"$src" "$dest"
+done
